cf2b3609527d355dbe019ae7c40f4e03bd98b583
xiayulin123 <xiayulinsky@gmail.com>
2024-01-16 21:12:14 -0500

    fix name

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type book struct {
+type tool struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Price    int    `json:"price"`
 	Quantity int    `json:"quantity"`
 }
 
-var tools = []book{
+var tools = []tool{
 	{ID: "1", Name: "Eraser", Price: 2, Quantity: 10},
 	{ID: "2", Name: "Pen", Price: 4, Quantity: 9},
 	{ID: "3", Name: "Pencil", Price: 3, Quantity: 6},
@@ -23,6 +23,9 @@ func getTools(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tools)
 }
 
+func addTools(c *gin.Context) {
+	var newBook tool
+}
 func main() {
 	router := gin.Default()
 	router.GET("/tools", getTools)
